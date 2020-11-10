f32dba4cd3e8bfd8d423dc6565ba274fd2f6b67a
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-10 09:40:53 +0100

    Update sum_functions.py

--- sum_functions.py
+++ sum_functions.py
@@ -70,7 +70,7 @@ def get_summary(article_text, limit_percent=0.1):
             break 
     
     reading_time(article_text)
-    st.write("Readability: ") , st.write(textstat.text_standard(article_text, float_output=False)) , st.write("([Flesch Reading Ease:](https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests#Flesch_reading_ease) ") , st.write(textstat.flesch_reading_ease(article_text)) , st.write(")")
+    st.write("Readability: ") + st.write(str(textstat.text_standard(article_text, float_output=False))) + st.write("([Flesch Reading Ease:](https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests#Flesch_reading_ease) ") + st.write(str(textstat.flesch_reading_ease(article_text))) + st.write(")")
     st.write("&#35; of sentences (pre):  {}".format(len(list(doc.sents))))
     st.write("&#35; of sentences (post): {}".format(len(summary)))
     #st.write("\n")
