3ce68f75d05331e3c08e4bed1a2c8451aad541c0
eraser070 <62172283+eraser070@users.noreply.github.com>
2020-11-18 16:14:52 +0100

    Update home.py (#5)

--- pages/home.py
+++ pages/home.py
@@ -14,6 +14,9 @@ def write():
     st.write("This is a project in which we try to differentiate between positive and negative/neutral news from the web automatically. For this we use a variety of machine learning models and dictionary based apporaches to sentiment analysis. Furthermore the goal of this project was to categorize (e.g. business, finance, sports etc.) and summarize news articles. The finished product could either be a website on which users could find only positive news, or a livefeed, where our project could serve as a helpful tool for journalists or similar professions.")
     #st.markdown("![Front Gif](https://motionandmomentum.files.wordpress.com/2017/10/5181a5b8-6e3d-466f-9cea-8f900375fd42-720- 000000640b9c2bac.gif)")
     
+    st.write('On this WebApp you will find an analyser page, on which you can try out our sentiment analysis, categorization and the extractive summary. Furthermore we included a preview of what a live newsfeed could look like in later stages of develpoment.')
+
+    st.write('Thank you for being interested in our project and now enjoy some good news (hopefully)')
 
 if __name__ == "__main__":
     write()
