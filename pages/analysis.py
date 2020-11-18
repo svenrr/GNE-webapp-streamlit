e7ee68cccd60fb9ce77218e06f5c93430ff82926
eraser070 <62172283+eraser070@users.noreply.github.com>
2020-11-18 11:00:37 +0100

    Add files via upload

--- pages/analysis.py
+++ pages/analysis.py
@@ -27,19 +27,16 @@ While the overall energy costs of this recycling process are lower, researchers 
     
     # Sidebar
     st.sidebar.title('Features')
-    st.sidebar.info('If you want to see some more details you can chose them here')
-    tokenization = st.sidebar.checkbox("Show Tokenization")
-    readability = st.sidebar.checkbox("Show readablity of the article")
-    word_frequency = st.sidebar.checkbox("Show word frequency")
-    reading_time = st.sidebar.checkbox("Show reading time")
+    st.sidebar.info('You can chose a few more features that will be displayed if you want to')
+    show_tokenization = st.sidebar.checkbox("Show Tokenization")
+    show_readability = st.sidebar.checkbox("Show readability of the article")
+    show_word_frequency = st.sidebar.checkbox("Show word frequency")
+    show_reading_time = st.sidebar.checkbox("Show reading time")
        
     ### Main Functions ###
     
     user_input_per = st.slider('Here you can chose by how much percent the article should be reduced for the summary', min_value=0.1, max_value=0.9, value=0.9, step=0.05)
     if st.button('Analyse'):
-        
-        if tokenization:
-            st.write('**Tokens:**', text.lem_text())
         
         # Sentiment
         result = analyse(text.transform())
@@ -56,10 +53,26 @@ While the overall energy costs of this recycling process are lower, researchers 
         # Summary
         summary = get_summary(user_input, (1-user_input_per))
         st.write('**Extractive Summary:**')
+        st.write(get_tags(user_input))
+        
         for i in range(len(summary)): 
             st.write("- ",summary[i],"\n")
-            
-        st.write(get_tags(user_input)+'**')
     
+    
+        # Features
+        if show_tokenization:
+            st.write('**Tokens:**', text.lem_text())
+            
+        if show_readability:
+            st.write('**Readability:**', readability(user_input))
+            
+        if show_word_frequency:
+            st.write('**Word frequency:**', word_frequency(user_input))
+            
+        if show_reading_time:
+            st.write('**Reading time:**', str(reading_time(user_input)) + ' min')
+        
+        
+        
 if __name__ == "__main__":
     write()
