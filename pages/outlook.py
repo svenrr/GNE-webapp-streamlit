a42a5b118a06e22766390954f6963ce95f9c8508
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-19 14:38:10 +0100

    Create outlook.py

---
+++ pages/outlook.py
@@ -0,0 +1,14 @@
+import logging
+import streamlit as st
+import awesome_streamlit as ast
+from awesome_streamlit.core.services import resources
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+
+
+def write():
+    pass
+
+    
+if __name__ == "__main__":
+    write()
