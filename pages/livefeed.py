69723370a008f93b9629c177a6c8561c9bd0dd0d
eraser070 <62172283+eraser070@users.noreply.github.com>
2020-11-19 09:48:18 +0100

    Add files via upload

--- pages/livefeed.py
+++ pages/livefeed.py
@@ -13,13 +13,16 @@ def write():
     num_articles = 1
     st.title('Newsfeed Preview')
     
+    st.sidebar.title('Information')
+    st.sidebar.info('In the future we would like to include filters for the feed. For example to only show one sentiment, certain categories or publishers etc.')
+    
     col_input, col_control= st.beta_columns((1,5))
     with col_input:
         num_articles = st.number_input('Enter the desired number of articles you want to see (1 to 100)', min_value=1, max_value = 100, value=10)
         
     
     if st.button('Generate feed'):
-        df = pd.read_csv('datasets/news_actual_clean.csv')
+        df = pd.read_csv('data/news_actual_preprocessed.csv')
         
     
         col1, col2, col3= st.beta_columns((3,0.2,3))
@@ -27,11 +30,11 @@ def write():
             for idx in range(int(num_articles/2)):
                 title_text = f'**[{df.iloc[idx].title}]({df.iloc[idx].url})**'
                 st.markdown(title_text)
-                if df.iloc[idx].Sentiment == 0:
+                if df.iloc[idx].sentiment == 0:
                     st.write('Sentiment: neutral/negative')
                 else:
                     st.write('Sentiment: positive')
-                st.write('Category:', df.iloc[idx].Category)
+                st.write('Category:', df.iloc[idx].category)
                 with st.beta_expander('Summary'):
                     summary = get_summary(df.iloc[idx].text)
                     for phrase in range(len(summary)): 
@@ -41,27 +44,16 @@ def write():
             for idx in range(int(num_articles/2), num_articles):
                 title_text = f'**[{df.iloc[idx].title}]({df.iloc[idx].url})**'
                 st.markdown(title_text)
-                if df.iloc[idx].Sentiment == 0:
+                if df.iloc[idx].sentiment == 0:
                     st.write('Sentiment: neutral/negative')
                 else:
                     st.write('Sentiment: positive')
-                st.write('Category:', df.iloc[idx].Category)
+                st.write('Category:', df.iloc[idx].category)
                 with st.beta_expander('Summary'):
                     summary = get_summary(df.iloc[idx].text)
                     for phrase in range(len(summary)): 
                         st.write("- ",summary[phrase],"\n")
         
-        
-    
-    #st.dataframe(df[0:num_articles])
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 if __name__ == "__main__":
     write()
