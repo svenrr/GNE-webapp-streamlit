869947d6d8be83adc948a8e18eb4cadac4857539
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-20 11:13:30 +0100

    Update home.py
    Updated the text

--- pages/home.py
+++ pages/home.py
@@ -11,14 +11,20 @@ logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 def write():
     """Writes content to the app"""
     st.title('Good News Everyone')
-    st.markdown("![Good News Everyone](./data/Good news everyone (1).png)")
+    st.write("The world is better than most people think! Approximately 90% of news on the web are negative and this results in a distorted view of the world.")
+    st.markdown("![Good News Everyone](GNE-webapp-streamlit/data/Good news everyone (1).png)")
     #<p align="center"><img src="" alt="logo" width="300"/></p>
-    st.write("This is a project in which we try to differentiate between positive and negative/neutral news from the web automatically. For this we use a variety of machine learning models and dictionary based apporaches to sentiment analysis. Furthermore the goal of this project was to categorize (e.g. business, finance, sports etc.) and summarize news articles. The finished product could either be a website on which users could find only positive news, or a livefeed, where our project could serve as a helpful tool for journalists or similar professions.")
+    st.write('''This is a project in which we try to differentiate between positive and negative/neutral news from the web automatically. 
+    For this we use a variety of machine learning models and dictionary based approaches to sentiment analysis. 
+    Furthermore the goal of this project was to categorize (e.g. business, finance, sports etc.) and summarize news articles. 
+    On this WebApp you will find an analyser page, on which you can try out our sentiment analysis, categorization and the extractive summary. Furthermore we included a preview of what a live newsfeed could look like in later stages of development.
+    Thank you for being interested in our project and now enjoy some good news (hopefully).''')
+    st.write('''General problems about news:
+    * Opinions are very subjective, whether something is good or bad 
+    * Articles can belong to several categories 
+    * “bad” news are more entertaining''')
+    
     #st.markdown("![Front Gif](https://motionandmomentum.files.wordpress.com/2017/10/5181a5b8-6e3d-466f-9cea-8f900375fd42-720- 000000640b9c2bac.gif)")
     
-    st.write('On this WebApp you will find an analyser page, on which you can try out our sentiment analysis, categorization and the extractive summary. Furthermore we included a preview of what a live newsfeed could look like in later stages of develpoment.')
-
-    st.write('Thank you for being interested in our project and now enjoy some good news (hopefully)')
-
 if __name__ == "__main__":
     write()
