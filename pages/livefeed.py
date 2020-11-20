9a208b74e9db5f89177d0ec173380d0134b5d0cb
eraser070 <62172283+eraser070@users.noreply.github.com>
2020-11-20 11:23:09 +0100

    Add files via upload

--- pages/livefeed.py
+++ pages/livefeed.py
@@ -13,16 +13,38 @@ def write():
     st.title('Newsfeed Preview')
     
     st.sidebar.title('Information')
-    st.sidebar.info('In the future we would like to include filters for the feed. For example to only show one sentiment, certain categories or publishers etc.')
+    st.sidebar.info('In the future we could include more filters (e.g. date of publishing)')
+    sentiment_choice = st.sidebar.multiselect('Select Sentiment', ['good', 'neutral/bad'], default=['good'])
+    category_choice = st.sidebar.multiselect('Select Category', ['entertainment', 'financial', 'politics', 'sports', 'technology', 'travel', 'world'])
+
+    
+    for idx, sentiment in enumerate(sentiment_choice):
+        if sentiment == 'good':
+            sentiment_choice[idx] = 1
+        if sentiment== 'neutral/bad':
+            sentiment_choice[idx] = 0
+
     
     col_input, col_control= st.beta_columns((2,4)) # define 2 columns to control size of input box
     with col_input:
-        num_articles = st.number_input('Enter the desired number of articles you want to see (1 to 100)', min_value=1, max_value = 100, value=10)
+        num_articles = st.number_input('Enter the desired number of articles you want to see', min_value=1, value=10)
         
     
     if st.button('Generate feed'):
         df = pd.read_csv('data/news_actual_preprocessed.csv')
         
+        if not sentiment_choice:
+            sentiment_choice = [0,1]
+        
+        if not category_choice:
+            category_choice = ['entertainment', 'financial', 'politics', 'sports', 'technology', 'travel', 'world']
+        
+        
+        df = df.query(f'sentiment == {sentiment_choice}')
+        df = df.query(f'category == {category_choice}')
+        
+        if df.shape[0] <= num_articles:
+            num_articles = df.shape[0]
     
         col1, col2, col3= st.beta_columns((3,0.2,3)) # generate 3 columns where the middle one functions as a seperator
         with col1:
