554826dbd865bc41d39d84d585dcfa29e29c57d1
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-23 08:54:12 +0100

    Update word_frequency.py
    Added comments

--- Functions/word_frequency.py
+++ Functions/word_frequency.py
@@ -1,3 +1,5 @@
+##### Imports & Setup #####
+
 import spacy
 from collections import Counter
 from string import punctuation
@@ -13,12 +15,17 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 nlp = en_core_web_sm.load()
 
+##### Function #####
+
 def word_frequency(article_text):
-    from spacy.lang.en.stop_words import STOP_WORDS
+    '''Input: An article text
+    Output: A summarization'''
+    from spacy.lang.en.stop_words import STOP_WORDS # Get english stopwords from spaCy
     stopwords = list(STOP_WORDS)
     
     doc = nlp(article_text.lower()) 
     
+    # Count the word frequency and save it in a dictionary 
     word_freqs = {}
     for word in doc:
         if (word.text not in stopwords and word.text not in punctuation and word.text not in '“”‘’'):
@@ -26,23 +33,19 @@ def word_frequency(article_text):
                 word_freqs[word.text] = 1
             else:
                 word_freqs[word.text] += 1
-                
+    
+    # Sort the dictionary in descending order starting from the most common word
     sort_orders = sorted(word_freqs.items(), key=lambda x: x[1], reverse=True)
-    #st.write("---"*40)
     st.write("**Words that appear more than three times:**")
 
-    #for i in sort_orders:
-    #    if i[1] > 3:
-    #        st.write(i[0], i[1])
-
-    # Save the results in a dataframe and plot it
+    # Save the results in a dataframe but only the words that occur more than 2 times
     wf_dic = dict()
     for i in sort_orders: 
         if i[1] > 3:
             wf_dic.update({i[0] : i[1]})
     
     wf_df = pd.DataFrame(wf_dic, index=["Word Frequencies"])
-    st.dataframe(wf_df) 
+    st.dataframe(wf_df) # Display the dataframe on the webapp 
     
     # Create and generate a word cloud image:
     wc_txt = ""
@@ -52,8 +55,8 @@ def word_frequency(article_text):
         
     wordcloud = WordCloud(background_color='white').generate(wc_txt)
 
-    # Display the generated image:
+    # Display the generated image/wordcloud:
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
-    st.pyplot()
+    st.pyplot() # Display the wordlcoud on the webapp 
