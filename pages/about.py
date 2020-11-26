f50b989210c4b491a14e9d9daad155fe4698770b
eraser070 <62172283+eraser070@users.noreply.github.com>
2020-11-26 14:47:29 +0100

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -9,7 +9,7 @@ def write():
     """Used to write the page in the app.py file"""
   
     with st.spinner("Loading About ..."):
-        ![Good News](https://motionandmomentum.files.wordpress.com/2017/10/5181a5b8-6e3d-466f-9cea-8f900375fd42-720-000000640b9c2bac.gif)
+        st.markdown("![Good News](https://motionandmomentum.files.wordpress.com/2017/10/5181a5b8-6e3d-466f-9cea-8f900375fd42-720-000000640b9c2bac.gif)")
         st.title("About us")
         st.markdown(
             """## Project
